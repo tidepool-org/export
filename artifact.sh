ad5d28f4b025ed8d6dc4b598450e42352bce241d
Ydris Rebibane <ydris@diabeloop.fr>
2021-09-01 14:14:25 +0200

    YLP-213 Remove_artifactsh (#18)
    * Remove artifact.sh
    
    * bump dependencies

--- artifact.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-wget -q -O artifact_node.sh 'https://raw.githubusercontent.com/mdblp/tools/dblp/artifact/artifact_node.sh'
-chmod +x artifact_node.sh
-
-. ./version.sh
-./artifact_node.sh
