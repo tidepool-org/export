55dfdc68b84f8190807ba1ae257bd03dd609f067
Chris McGee <chris@tidepool.org>
2026-06-17 14:15:10 -0400

    also bump esbuild config target

--- esbuild.config.js
+++ esbuild.config.js
@@ -7,7 +7,7 @@ esbuild
     bundle: true,
     outfile: './dist/app.cjs',
     platform: 'node',
-    target: 'node20',
+    target: 'node24',
     inject: ['./esbuild-patch.cjs'], // https://github.com/evanw/esbuild/issues/1492#issuecomment-893144483
     define: {
       'import.meta.url': 'import_meta_url',
